09e4ddc25a098edd5702df7669eeca3cdaf0e25d
AlvinNorin <alvin@norin.family>
2025-08-26 00:28:21 +0200

    Add ProtectedLocation class for managing protected areas with exclusion radius and neighbor relationships

---
+++ src/io/github/alvinnorin/steinerportals/ProtectedLocation.java
@@ -0,0 +1,58 @@
+package io.github.alvinnorin.steinerportals;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.Location;
+
+import java.util.*;
+
+public class ProtectedLocation {
+
+    private final int exclusionRadius = 5000;    // The exclusion radius every protected location starts off with
+
+    @Getter
+    private UUID world;
+
+    @Getter
+    private int x;
+
+    @Getter
+    private int z;
+
+    @Getter @Setter
+    private UUID owner;
+
+    @Getter
+    private final HashSet<ProtectedLocation> neighbours = new HashSet<>();
+
+    public ProtectedLocation(UUID world, int x, int y, UUID owner) {
+        this.world = world;
+        this.x = x;
+        this.z = y;
+        this.owner = owner;
+    }
+
+    public ProtectedLocation(Location location) {
+        this.world = Objects.requireNonNull(location.getWorld()).getUID();
+        this.x = location.getBlockX();
+        this.z = location.getBlockZ();
+    }
+
+    public int getProtectionRadius() {
+        return exclusionRadius + (neighbours.size() * exclusionRadius);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, z, world);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        ProtectedLocation that = (ProtectedLocation) obj;
+        return x == that.x && z == that.z && world.equals(that.world);
+    }
+
+}
